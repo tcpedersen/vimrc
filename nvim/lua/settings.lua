ece658b86f860559c302adc7da8e65c575084f8c
tcpedersen <tobiascramerpedersen@gmail.com>
2024-06-05 09:30:19 +0200

    Display LSP error messages when hovering.

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -11,3 +11,10 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
+vim.diagnostic.config({
+  virtual_text = false
+})
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
